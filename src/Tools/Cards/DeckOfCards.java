46bb230a1fa1d96020c61ef3195417319a834377
Leo <quentin.coui@hotmail.com>
2020-04-13 11:16:44 +0200

    Feat refactor
    Second refactor for the sake of the Api

--- src/Tools/Cards/DeckOfCards.java
+++ src/Tools/Cards/DeckOfCards.java
@@ -37,6 +37,25 @@ public class DeckOfCards {
        
     }
     
+    public void init_deck() throws IOException
+    {
+        String [] faces = {"2","3","4","5","6","7","8","9","10","J", "Q", "K","A"};
+        String [] suits = {"Spades","Diamonds","Hearts","Clubs"}; 
+        deck = new Card[deckLength];
+        
+        for (int suit = 0; suit < suits.length; suit++) {
+            for (int face = 0; face < faces.length; face++) {
+                int index = face + (suit *13);
+                String current_face = faces[face]; 
+                String current_suit = suits[suit];
+                
+                deck[index] = new Card(current_face, current_suit, face+2, ImageIO.read(new File("Cards_img/"+current_face+current_suit.substring(0,1)+".png")));
+            }
+            
+        }
+        
+    }
+    
     public void display_deck()
     {
         for(Card card : deck)
@@ -51,6 +70,18 @@ public class DeckOfCards {
     public Card getCardWithIndex(int x)
     {
         return deck[x];
+    }
+    
+    public Card getCardWithFaceAndValue(String suit, String face)
+    {
+        Card target; 
+        for (Card card:deck) {
+            if (card.getFace() == face && card.getSuit() == suit) {
+                target = card; 
+                return target; 
+            }
+        }
+        return null; 
     }
     
     public void shuffle()
@@ -79,26 +110,6 @@ public class DeckOfCards {
             return deck[cursor++];
         }   
     }
-    
-    public void init_deck() throws IOException
-    {
-        String [] faces = {"2","3","4","5","6","7","8","9","10","J", "Q", "K","A"};
-        String [] suits = {"Spades","Diamonds","Hearts","Clubs"}; 
-        deck = new Card[deckLength];
-        
-        for (int suit = 0; suit < suits.length; suit++) {
-            for (int face = 0; face < faces.length; face++) {
-                int index = face + (suit *13);
-                String current_face = faces[face]; 
-                String current_suit = suits[suit];
-                
-                deck[index] = new Card(current_face, current_suit, face+2, ImageIO.read(new File("Cards_img/"+current_face+current_suit.substring(0,1)+".png")));
-            }
-            
-        }
-        
-    }
-    
     
     
 }
